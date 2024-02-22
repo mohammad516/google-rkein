b30a60b3c1056c5aa23a61791a0bfed8f0172205
Mohamad Rkein <mohammadrkein79@gmail.com>
2024-02-22 16:55:23 +0200

    Create Search page for web and image

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,13 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        pathname: "**",
+        protocol: "https", // Protocol for the remote image URLs
+        hostname: "upload.wikimedia.org", // Hostname for Wikimedia Commons
+        pathname: "**", // Match all pathnames under Wikimedia Commons
       },
     ],
   },
 };
 
 export default nextConfig;
+
---
+++ src/app/search/image/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ImageSearchPage() {
+  return (
+    <div className='text-red-400'>ImageSearchPage</div>
+  )
+}
